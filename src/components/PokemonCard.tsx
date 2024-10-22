334e775436ec70ba25520022b2e1b139f5e6d999
BELHASSEN <julia_belhassen@hotmail.com>
2024-10-22 16:38:13 +0200

    add my pokemon

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,15 +1,13 @@
-import "./App.css";
-
-import MyTitle from "./components/PokemonCard";
-
-function App() {
+function PokemonCard() {
   return (
+  <>
     <figure>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="un Pokemon">
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="un Pokemon" />
       <figcaption>Bulbasaur</figcaption>
-      <PokemonCard />
     </figure>
+  </>
+
   );
 }
 
-export default App;
+export default PokemonCard;
---
+++ src/components/Salameche.tsx
@@ -0,0 +1,13 @@
+function Salameche() {
+  return (
+  <>
+    <figure>
+      <img src="https://i.pinimg.com/enabled_lo/564x/1a/bf/4b/1abf4b00ee767b8ebb5aa57bdc48627b.jpg" alt="un Salameche" />
+      <figcaption>Bulbasaur</figcaption>
+    </figure>
+  </>
+
+  );
+}
+
+export default Salameche;
