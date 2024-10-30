01fc6c3d887b06e90dc1933c212d204ece74b02e
BELHASSEN <julia_belhassen@hotmail.com>
2024-10-30 16:36:34 +0100

    Quete 3 de React

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import PokemonCard from "./components/PokemonCard"
 
+
+
 function App() {
+
+
   return (
     <>
-      <h3>COUCOU</h3>
+     <main>
+      <h3>POKEMON</h3>
+
       <PokemonCard />
+    
+     </main>
     </>
 
   );
 }
+
+
+// Une props est une propriete qu'on donne a l'un de nos composants, comme un attribut HTML. 
+// On passe les props toujours du parent vers l'enfant
+// exemple <Card firstname = "Mario"/>
 
 export default App;
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,13 +1,35 @@
-function PokemonCard() {
-  return (
-  <>
+  // interface cardId {
+  // name: string;
+  // imgSrc: string;
+  // }
+
+
+  function PokemonCard () {
+
+    const pokemon = pokemonList[1]
+
+    return (
+      <>
     <figure>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="un Pokemon" />
-      <figcaption>Bulbasaur</figcaption>
+      {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>} 
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   </>
-
   );
 }
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+
+  {
+    name: "mew",
+    imgSrc: ""
+  },
+
+];
 
 export default PokemonCard;
