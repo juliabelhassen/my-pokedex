46d6e3b23f39e754aa56654db47a51f732fccb1a
BELHASSEN <julia_belhassen@hotmail.com>
2024-10-31 10:51:07 +0100

    Props.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import PokemonCard from "./components/PokemonCard"
+import PokemonCard from "./components/PokemonCard";
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
 
+  {
+    name: "mew",
+    imgSrc: "",
+  },
+];
 
 function App() {
-
-
   return (
     <>
-     <main>
-      <h3>POKEMON</h3>
+      <main>
+        <h3>POKEMON</h3>
 
-      <PokemonCard />
-    
-     </main>
+        <PokemonCard pokemon={pokemonList[0]} />
+      </main>
     </>
-
   );
 }
 
-
-// Une props est une propriete qu'on donne a l'un de nos composants, comme un attribut HTML. 
+// Une props est une propriete qu'on donne a l'un de nos composants, comme un attribut HTML.
 // On passe les props toujours du parent vers l'enfant
 // exemple <Card firstname = "Mario"/>
 
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,35 +1,17 @@
-  // interface cardId {
-  // name: string;
-  // imgSrc: string;
-  // }
+// interface cardId {
+// name: string;
+// imgSrc: string;
+// }
 
-
-  function PokemonCard () {
-
-    const pokemon = pokemonList[1]
-
-    return (
-      <>
-    <figure>
-      {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>} 
-      <figcaption>{pokemon.name}</figcaption>
-    </figure>
-  </>
+function PokemonCard({ pokemon }) {
+  return (
+    <>
+      <figure>
+        {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>}
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    </>
   );
 }
-
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-
-  {
-    name: "mew",
-    imgSrc: ""
-  },
-
-];
 
 export default PokemonCard;
